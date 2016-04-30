6fde1d6795a85e664ff156a7f8ca2e9a3edfdac3
SevenBits <meokid2@yahoo.com>
2016-04-29 23:48:04 -0400

    Detect user and system information
    Make sure that the user is running on Linux, and quit with an
    error if we are not.
    
    This commit also makes the script executable.

--- unh_wifi.sh
+++ unh_wifi.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
 # Contribution of the President, Keith M. Hoodlet, 04.13.2016
+#
+# Written by the UNH Linux Club
+read_passwd() {
+	stty -echo
+	read -p "Password: " passw; echo
+	stty echo
+}
+
+check_your_privilege() {
+	if [ "$(whoami)" != 'root' ]; then
+		echo "You have no permission to run $0 as non-root user."
+		exit 1;
+	fi
+}
+
+check_reqs() {
+	set OS_NAME = `uname`
+	if [ ! $OS_NAME == "Linux" ]
+	then
+		return 0
+	fi
+
+	return 1
+}
+
+main() {
+	echo "======================================"
+	echo "===== UNH Easy WiFi Setup Script ====="
+	echo "======================================"
+	echo ""
+
+	#check_your_privilege
+	check_reqs
+	if [ $? -eq 0 ]
+	then
+		echo "Error: distribution not supported."
+		exit 1
+	fi
+	
+	echo -n "System Information: "
+	echo "`uname` `uname -r`"
+}
+
+# Run the script.
+main
+exit 0
