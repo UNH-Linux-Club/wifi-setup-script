f9926e59e7d5746fcb313c2725cc873aacfd4c6c
SevenBits <meokid2@yahoo.com>
2016-04-30 00:31:00 -0400

    User username and password for UNH and elevate if needed

--- unh_wifi.sh
+++ unh_wifi.sh
@@ -8,16 +8,19 @@ read_passwd() {
 	stty echo
 }
 
-check_your_privilege() {
-	if [ "$(whoami)" != 'root' ]; then
-		echo "You have no permission to run $0 as non-root user."
-		exit 1;
-	fi
-}
-
 check_reqs() {
-	set OS_NAME = `uname`
+	# Make sure we're running Linux and not on a Mac
+	OS_NAME=`uname`
 	if [ ! $OS_NAME == "Linux" ]
+	then
+		return 0
+	fi
+
+	# Check if we're running a supported distro
+	# TODO: Don't assume /etc/issue exists
+	IS_MINT=$( grep -ic "Linux Mint" /etc/issue )
+	IS_UBUNTU=$( grep -ic "Ubuntu" /etc/issue )
+	if [[ ! $IS_MINT -eq 1 ]] && [[ ! $IS_UBUNTU -eq 1 ]]
 	then
 		return 0
 	fi
@@ -25,24 +28,51 @@ check_reqs() {
 	return 1
 }
 
-main() {
+intro() {
 	echo "======================================"
 	echo "===== UNH Easy WiFi Setup Script ====="
 	echo "======================================"
 	echo ""
 
-	#check_your_privilege
 	check_reqs
 	if [ $? -eq 0 ]
 	then
 		echo "Error: distribution not supported."
 		exit 1
 	fi
-	
+
 	echo -n "System Information: "
 	echo "`uname` `uname -r`"
 }
 
-# Run the script.
-main
+get_info() {
+	read -p "Username: " user
+	stty -echo
+	read -p "Password: " passw; echo
+	stty echo
+
+	# Don't actually do this in production.
+	echo "User: $user Password: $passw"
+}
+
+# Run the script and cx
+if [ $EUID != 0 ]
+then
+	intro
+
+	echo "Root access is needed to run this script. Attempting to elevate permissions..."
+	echo ""
+	exec sudo "$0" "$@"
+else
+	echo "======================================"
+	echo "This script will attempt to automatically setup the WiFi connection"
+	echo "on your new Linux PC. You will need your UNH username and password"
+	echo "(same as Blackboard)."
+	echo ""
+
+	get_info
+fi
+
+# Get user input.
+
 exit 0
