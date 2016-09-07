1c04a268fc223d82d2f26dc88a38f5d5cbb1d61a
Jeremy Plsek <jeremyplsek@gmail.com>
2016-09-07 18:03:43 -0400

    This should now work after changing the wireless iterface (a TODO I will put in next commit)
    Removed checks for Ubuntu based systems, because everyone uses
    NetworkManager if they don't know how to configure their wireless
    interfaces by themselvels.
    Removed serveral debug statements.
    Removed the password prompt, as it is not necessary for this
    implenetation.
    Removed root privledges as we don't need them yet.
    Restructured some code to use a main function instead of daisy chaining
    functions.

--- unh_wifi.sh
+++ unh_wifi.sh
@@ -10,15 +10,6 @@ check_reqs() {
 		return 0
 	fi
 
-	# Check if we're running a supported distro
-	# TODO: Don't assume /etc/issue exists
-	IS_MINT=$( grep -ic "Linux Mint" /etc/issue )
-	IS_UBUNTU=$( grep -ic "Ubuntu" /etc/issue )
-	if [[ ! $IS_MINT -eq 1 ]] && [[ ! $IS_UBUNTU -eq 1 ]]
-	then
-		return 0
-	fi
-
 	return 1
 }
 
@@ -34,70 +25,60 @@ intro() {
 		echo "Error: distribution not supported."
 		exit 1
 	fi
-
-	echo -n "System Information: "
-	echo "`uname` `uname -r`"
 }
 
-start_network() {
-	echo -n "Starting network manager... "
-	STATUS=$( start network-manager )
-	if [[ ! $STATUS -eq 0 ]]
-	then
-		echo "failed."
+run_nmcli() {
+	echo "Creating UNH-Secure profile..."
+	nmcli con add type wifi ifname wlp3s0 con-name UNH-Secure ssid UNH-Secure
+
+	echo "Editing UNH-Secure profile..."
+	nmcli con edit UNH-Secure <<EOF
+set 802-1x.eap peap
+set 802-1x.identity $user
+set 802-1x.phase2-auth mschapv2
+set 802-11-wireless-security.auth-alg open
+set 802-11-wireless-security.key-mgmt wpa-eap
+save
+quit
+EOF
+
+	if [ $? != 0 ]; then
 		exit 1
 	fi
 }
 
 check_if_already_registered() {
-	echo -n "Scanning registered networks... "
 	if [ -f /etc/NetworkManager/system-connections/UNH-Secure ]
 	then
-		echo "failed!"
 		echo "There is already a network named UNH-Secure registered with the system."
 		echo "Please forget it and then try again."
 		exit 1
-	else
-		echo "done!"
+		# TODO ask user if they would like us to delete it for them
 	fi
 }
 
 get_info() {
 	read -p "Username: " user
-	stty -echo
-	read -p "Password: " passw; echo
-	stty echo
-
-	# Don't actually do this in production.
-	echo "User: $user Password: $passw"
-	start_network
-	check_if_already_registered
+	echo
 }
 
-# Run the script and attempt to elevate if we can.
-# We only print the opening intro if they're running as a standard
-# user. If they're running as root, we skip it and go directly to
-# the setup. This serves two purposes:
-#
-# a) it simplifies reducing duplicated code when the script is
-#    launched again via sudo; and
-# b) if they're advanced enough to be "taking the plunge" as root,
-#    we can probably spare them noobish treatment.
-if [ $EUID != 0 ]
-then
+main() {
 	intro
-
-	echo "Root access is needed to run this script. Attempting to elevate permissions..."
-	echo ""
-	exec sudo "$0" "$@"
-else
 	echo "======================================"
 	echo "This script will attempt to automatically setup the WiFi connection"
-	echo "on your new Linux PC. You will need your UNH username and password"
+	echo "on your new Linux PC. You will need your UNH username."
 	echo "(same as Blackboard)."
 	echo ""
 
+	# We can assume they are using netowrk manager
+	check_if_already_registered
 	get_info
-fi
+	run_nmcli
+
+	echo
+	echo "IMPORTANT: Restart your system, then try to connect to UNH-Secure via the wifi menu. Good luck."
+}
+
+main
 
 exit 0
